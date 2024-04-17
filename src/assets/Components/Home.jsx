50c449a7944767d3f8d128b007d8b963db856c5e
Deepak Singhal <Deepaksinghal0320@gmail.com>
2024-04-17 08:40:34 +0530

    Added some changes in resume and bg-img

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -10,11 +10,11 @@ function Home() {
     return (
         <>
             <div className='homeBackground '>
-                <div className='homeTitle '>
+                <div className='homeTitle'>
                     <p>Hello World!</p>
-                    <p>I'm, { }{text}{ } <Cursor /> </p>    
+                    <p>I'm, { }{text}{ } <Cursor /> </p>
                     <p>from India.</p>
-                    <button type="button" className="btn btn-outline-discovery">Resume</button>
+                    <button type="button" className="btn btn-outline-discovery"><a href='https://docs.google.com/document/d/18_V250IADsx-kcSnWiwkeSYmFpX_1ceOqvPhbkir3G8/edit?usp=sharing' target='_blank'>Resume</a></button>
                 </div>
             </div>
         </>
